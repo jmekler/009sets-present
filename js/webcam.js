0925a4537436cc102221222ca646a727955b3cce
Jeff Mekler <jmekler@gmail.com>
2016-12-11 15:47:27 -0500

    page updates

---
+++ js/webcam.js
@@ -0,0 +1,40 @@
+if ($('.webcam').length) {
+	// add video element to webcam
+	let $video = $("<video data-autoplay=true></video>");
+	let video = $video.get(0);
+	$('.webcam').append($video);
+	
+	if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+		console.log("enumerateDevices() not supported.");
+	}
+	
+	// list out video sources
+	navigator.mediaDevices.enumerateDevices()
+		.then(function(devices) { 
+			let deviceId = ""
+			let cameras = devices.filter( (d)=> (d.kind == "videoinput"))
+			console.log("Available webcams:");
+			cameras.map( (device)=> console.log(device.kind + ":" + device.deviceId) )
+			
+			let constraints = {
+			    audio: false,
+			    video: {
+				    deviceId: cameras[1].deviceId,
+			        width: { min: 1280, ideal: 1920 },
+			        height: { min: 720, ideal: 1080 },
+			    }
+			};
+			
+			navigator.mediaDevices.getUserMedia(constraints)
+				.then(function(stream) {
+					let videoTracks = stream.getVideoTracks();
+					console.log(videoTracks);
+// 					let video = $('.webcam').children('video').get(0);
+					console.log('Using video device: ' + videoTracks[0].label);
+					stream.onended = function() { console.log('Stream ended'); };
+					window.stream = stream; // make variable available to console
+					video.srcObject = stream;
+				})
+				.catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+		});	
+}
